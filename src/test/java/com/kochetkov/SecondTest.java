d4ff022aae96fdb7b63e9f2670f5358d6bd79c37
IlyaKoch <kochetkov6363@gmail.com>
2023-02-13 21:51:58 +0400

    one commit out of two
    *added third test

---
+++ src/test/java/com/kochetkov/SecondTest.java
@@ -0,0 +1,12 @@
+package com.kochetkov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(true);
+        System.out.println("Second");
+    }
+}
---
+++ src/test/java/com/kochetkov/ThirdTest.java
@@ -0,0 +1,12 @@
+package com.kochetkov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ThirdTest {
+    @Test
+    void thirdTest() {
+        Assertions.assertTrue(true);
+        System.out.println("Third");
+    }
+}
